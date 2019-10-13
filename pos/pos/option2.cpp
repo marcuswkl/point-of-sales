6a971708e2ef1a2b7f0e8fd11211b4adee1b5ee2
marcuswkl <49618123+marcuswkl@users.noreply.github.com>
2019-10-14 01:42:53 +0800

    Create do...while loop, add do code and emove Group1.cpp
    Create do...while loop. add code to do, remove Group1.cpp to avoid errors

--- pos/pos/option2.cpp
+++ pos/pos/option2.cpp
@@ -4,7 +4,41 @@
 
 int main(void)
 {
-	
+	int noMoreItems = 0, wallScrapper = 0, tilesWaxes = 0, mudtarRemover = 0, dryBlower = 0, quantity;
+	char code[3];
+
+	//do {
+		printf("Please key in product code:\n");
+		scanf_s("%s", code);
+			if (code == "101") {
+				printf("Wall Scrapper\n");
+				printf("Please key in quantity:\n");
+				scanf_s("%d", &quantity);
+				wallScrapper += quantity;
+			}
+			else if (code == "202") {
+				printf("Tiles Waxes\n");
+				printf("Please key in quantity:\n");
+				scanf_s("%d", &quantity);
+				tilesWaxes += quantity;
+			}
+			else if (code == "303") {
+				printf("Mud/Tar Remover\n");
+				printf("Please key in quantity:\n");
+				scanf_s("%d", &quantity);
+				mudtarRemover += quantity;
+			}
+			else if (code == "404") {
+				printf("Dry Blower\n");
+				printf("Please key in quantity:\n");
+				scanf_s("%d", &quantity);
+				dryBlower += quantity;
+			}
+			else {
+				printf("Error 404 Product Not Found!");
+			}
+	//} while(condition);
+
 	_getch();
 	return 0;
 }
