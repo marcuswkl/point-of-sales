bd8f2e43b301cf65b6130cf2217b74f45ebf256e
marcuswkl <49618123+marcuswkl@users.noreply.github.com>
2019-10-14 00:05:47 +0800

    Add printf options and switch case
    Add printf options to print all 5 options and create switch case for those options, cases for options 1 to 4 are placeholders, default case can be looped in the future

--- pos/pos/Group1.cpp
+++ pos/pos/Group1.cpp
@@ -4,7 +4,31 @@
 
 int main(void)
 {
+	int option;
 
-	getch();
+	printf("What do you want to do?\n");
+	printf("Select an option below by inputting numbers:\n");
+	printf("Option 1: Product and Price details\n");
+	printf("Option 2: Calculate price of purchased items\n");
+	printf("Option 3 : Calculate the delivery charges\n");
+	printf("Option 4 : Calculate total pay amount\n");
+	printf("Option 5 : Exit\n");
+
+	scanf_s("%d", &option);
+	switch (option) {
+	case 1: printf("Option1");
+			break;
+	case 2: printf("Option2");
+			break;
+	case 3: printf("Option3");
+			break;
+	case 4: printf("Option4");
+			break;
+	case 5: exit(0);
+			break;
+	default: printf("404 Option Not Found!");
+	}
+
+	_getch();
 	return 0;
 }
