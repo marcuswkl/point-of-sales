b222cadb170eb779e7501ff21d24618f94061fd4
WaiS00 <56108922+WaiS00@users.noreply.github.com>
2019-10-13 17:18:32 +0800

    Q1 (1st Edit)

--- pos/pos/Group1.cpp
+++ pos/pos/Group1.cpp
@@ -5,6 +5,41 @@
 int main(void)
 {
 
+
+printf("  ______               __        ______        __                  _______         __ __ \n");
+printf("      __ \.-----.-----.|  |_     |   __ \.----.|__|.----.-----.     |   |   |.---.-.|  |  | \n");
+printf(" |   __ <|  -__|__ --||   _|    |    __/|   _||  ||  __|  -__|    |       ||  _  ||  |  | \n");
+printf(" |______/|_____|_____||____|    |___|   |__|  |__||____|_____|    |__|_|__||___._||__|__|\n\n");
+
+printf("Hi, we are the Best Price Mall which has the best price in the market. \n");
+printf("If you can find anything cheaper than our shop then..... \n");
+
+printf(" __  __ __           __ __             _______         __                __       \n");
+printf("|  |/  |__|.-----.--|  |  |.--.--.    |     __|.-----.|  |_.-----.--.--.|  |_     \n");
+printf("|     <|  ||     |  _  |  ||  |  |    |    |  ||  -__||   _|  _  |  |  ||   _|    \n");
+printf("|__|\__ |__||__|__|_____|__||___  |    |_______||_____||____|_____|_____||____|    \n");
+printf("                           |_____|                                                \n\n");
+
+char continue_name;
+int membership_status;
+
+
+printf("Please select your membership status :\n",membership_status);
+printf("1 ---> Member\n");
+printf("2 ---> Non-member\n\n");
+scanf("         %d",&membership_status);
+
+if (membership_status== 1)
+    {
+        printf("You're a member");
+    }
+
+else
+{
+    printf("You're a non member.");
+}
+
+
 	getch();
 	return 0;
 }
