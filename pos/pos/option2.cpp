7301215ce93c6c0b47fe46265780437654f408b5
marcuswkl <49618123+marcuswkl@users.noreply.github.com>
2019-10-14 17:19:48 +0800

    Complete do...while loop
    Finish do...while loop code

--- pos/pos/option2.cpp
+++ pos/pos/option2.cpp
@@ -4,40 +4,57 @@
 
 int main(void)
 {
-	int noMoreItems = 0, wallScrapper = 0, tilesWaxes = 0, mudtarRemover = 0, dryBlower = 0, quantity;
-	char code[3];
+	int code, noMoreItems, wallScrapper = 0, tilesWaxes = 0, mudtarRemover = 0, dryBlower = 0, quantity;
 
-	//do {
+	do {
 		printf("Please key in product code:\n");
-		scanf_s("%s", code);
-			if (code == "101") {
+		scanf_s("%d", &code);
+			if (code == 101) {
 				printf("Wall Scrapper\n");
 				printf("Please key in quantity:\n");
 				scanf_s("%d", &quantity);
 				wallScrapper += quantity;
+				printf("Do you have any more items? Please input a number to select an option:\n");
+				printf("0.Yes\n");
+				printf("1.No\n");
+				scanf_s("%d", &noMoreItems);
 			}
-			else if (code == "202") {
+			else if (code == 202) {
 				printf("Tiles Waxes\n");
 				printf("Please key in quantity:\n");
 				scanf_s("%d", &quantity);
 				tilesWaxes += quantity;
+				printf("Do you have any more items? Please input a number to select an option:\n");
+				printf("0.Yes\n");
+				printf("1.No\n");
+				scanf_s("%d", &noMoreItems);
 			}
-			else if (code == "303") {
+			else if (code == 303) {
 				printf("Mud/Tar Remover\n");
 				printf("Please key in quantity:\n");
 				scanf_s("%d", &quantity);
 				mudtarRemover += quantity;
+				printf("Do you have any more items? Please input a number to select an option:\n");
+				printf("0.Yes\n");
+				printf("1.No\n");
+				scanf_s("%d", &noMoreItems);
 			}
-			else if (code == "404") {
+			else if (code == 404) {
 				printf("Dry Blower\n");
 				printf("Please key in quantity:\n");
 				scanf_s("%d", &quantity);
 				dryBlower += quantity;
+				printf("Do you have any more items? Please input a number to select an option:\n");
+				printf("0.Yes\n");
+				printf("1.No\n");
+				scanf_s("%d", &noMoreItems);
 			}
 			else {
 				printf("Error 404 Product Not Found!");
 			}
-	//} while(condition);
+	} while(noMoreItems == 0);
+
+	printf("");
 
 	_getch();
 	return 0;
