76027f4507921c9e2671aed4e767101a03cab505
marcuswkl <49618123+marcuswkl@users.noreply.github.com>
2019-10-14 23:51:12 +0800

    Add calculation formulas and printf
    Add calculation formulas for subtotal of every product and grand total of all the products as well as printf the calculations

--- pos/pos/option2.cpp
+++ pos/pos/option2.cpp
@@ -4,7 +4,8 @@
 
 int main(void)
 {
-	int code, noMoreItems, wallScrapper = 0, tilesWaxes = 0, mudtarRemover = 0, dryBlower = 0, quantity;
+	int code, anymoreItems, wallScrapper = 0, tilesWaxes = 0, mudtarRemover = 0, dryBlower = 0, quantity;
+	double wallScrapperST, tilesWaxesST, mudtarRemoverST, dryBlowerST, productGrandTotal;
 
 	do {
 		printf("Please key in product code:\n");
@@ -15,9 +16,9 @@ int main(void)
 				scanf_s("%d", &quantity);
 				wallScrapper += quantity;
 				printf("Do you have any more items? Please input a number to select an option:\n");
-				printf("0.Yes\n");
-				printf("1.No\n");
-				scanf_s("%d", &noMoreItems);
+				printf("1.Yes\n");
+				printf("2.No\n");
+				scanf_s("%d", &anymoreItems);
 			}
 			else if (code == 202) {
 				printf("Tiles Waxes\n");
@@ -25,9 +26,9 @@ int main(void)
 				scanf_s("%d", &quantity);
 				tilesWaxes += quantity;
 				printf("Do you have any more items? Please input a number to select an option:\n");
-				printf("0.Yes\n");
-				printf("1.No\n");
-				scanf_s("%d", &noMoreItems);
+				printf("1.Yes\n");
+				printf("2.No\n");
+				scanf_s("%d", &anymoreItems);
 			}
 			else if (code == 303) {
 				printf("Mud/Tar Remover\n");
@@ -35,9 +36,9 @@ int main(void)
 				scanf_s("%d", &quantity);
 				mudtarRemover += quantity;
 				printf("Do you have any more items? Please input a number to select an option:\n");
-				printf("0.Yes\n");
-				printf("1.No\n");
-				scanf_s("%d", &noMoreItems);
+				printf("1.Yes\n");
+				printf("2.No\n");
+				scanf_s("%d", &anymoreItems);
 			}
 			else if (code == 404) {
 				printf("Dry Blower\n");
@@ -45,16 +46,26 @@ int main(void)
 				scanf_s("%d", &quantity);
 				dryBlower += quantity;
 				printf("Do you have any more items? Please input a number to select an option:\n");
-				printf("0.Yes\n");
-				printf("1.No\n");
-				scanf_s("%d", &noMoreItems);
+				printf("1.Yes\n");
+				printf("2.No\n");
+				scanf_s("%d", &anymoreItems);
 			}
 			else {
 				printf("Error 404 Product Not Found!");
 			}
-	} while(noMoreItems == 0);
+	} while(anymoreItems == 1);
 
-	printf("");
+	wallScrapperST = wallScrapper * 100;
+	tilesWaxesST = tilesWaxes * 100;
+	mudtarRemoverST = mudtarRemover * 100 * 0.8;
+	dryBlowerST = dryBlower * 100 * 0.75;
+	productGrandTotal = wallScrapperST + tilesWaxesST + mudtarRemoverST + dryBlowerST;
+
+	printf("Your subtotal for Wall Scrapper is RM%.2lf\n", wallScrapperST);
+	printf("Your subtotal for Tiles Waxes is RM%.2lf\n", tilesWaxesST);
+	printf("Your subtotal for Mud/Tar Remover is RM%.2lf\n", mudtarRemoverST);
+	printf("Your subtotal for Dry Blower is RM%.2lf\n", dryBlowerST);
+	printf("Your grand total for all the products is RM%.2lf\n", productGrandTotal);
 
 	_getch();
 	return 0;
